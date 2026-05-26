43891a7af815c4670d8b338a97eba3e06fb4474e
gabrii3lmao <gabrielmluz56@gmail.com>
2026-05-26 20:38:38 -0300

    installing PrimeVue on project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
 import "@/assets/main.css";
-import "primeicons/primeicons.css"
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from "primevue/config";
+import "primeicons/primeicons.css";
+import Aura from "@primevue/themes/aura";
 
 const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: ".my-app-dark",
+    },
+  },
+});
 app.use(router);
 app.mount("#app");
